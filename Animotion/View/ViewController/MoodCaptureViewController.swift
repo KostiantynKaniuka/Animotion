ccf01bee785980b197ea1b840db648aff4e91fc0
heyhocombo <heyhocombo@gmail.com>
2023-08-10 16:53:47 +0200

    before graph feature

---
+++ Animotion/View/ViewController/MoodCaptureViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MoodCaptureViewController.swift
+//  Animotion
+//
+//  Created by Kostiantyn Kaniuka on 10.08.2023.
+//
+
+import UIKit
+
+class MoodCaptureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
